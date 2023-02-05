c94b1c6562d799cae2995cf0cdfdcf9da61b7d8c
Andrey Grabarnick <andrey@hyperguest.com>
2023-02-05 09:33:52 +0200

    add some opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use cpu::Cpu;
+
+mod cpu;
+// mod keypad;
+// mod display;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = Cpu::new();
+
+    cpu.load_program("roms/pong.ch8");
+
+    'main : loop {
+        cpu.cycle();
+    }
 }
